76ac97a69b56fc7cbab05578e8a71c0d19f47dd6
guojuntao <guojuntao1@gmail.com>
2013-09-09 23:13:33 +0800

    13.09.09

--- ch3/ex3-01.c
+++ ch3/ex3-01.c
@@ -4,4 +4,23 @@
 // would suffice (at the price of more tests outside). Write a version with only
 // one test inside the loop and measure the difference in run-time.
 
-#include <stdio.h>
+int binsearch(int x, int v[], int n) {
+  int low = 0;
+  int high = n - 1;
+  int mid = (low + high) / 2;
+
+  while (low <= high && x != v[mid]) {
+    if (x < v[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+    mid = (low + high) / 2;
+  }
+
+  if (x == v[mid]) {
+    return mid;
+  } else {
+    return -1;
+  }
+}
--- ch3/ex3-02.c
+++ ch3/ex3-02.c
@@ -5,4 +5,73 @@
 // string t to s. Use a switch. Write a function for the other direction as
 // well, converting escape sequences into the real characters.
 
+#include <assert.h>
 #include <stdio.h>
+#include <string.h>
+
+int escape(char dest[], char src[]);
+int real(char dest[], char src[]);
+
+int main() {
+  char escape_string[40] = "first\\nsecond\\tthird";
+  char real_string[40] = "first\nsecond\tthird";
+  char compare_string[40];
+  escape(compare_string, real_string);
+  assert(!strcmp(escape_string, compare_string));
+  real(compare_string, escape_string);
+  assert(!strcmp(real_string, compare_string));
+  return 0;
+}
+
+int escape(char dest[], char src[]) {
+  int i = 0;
+  int j = 0;
+  while (src[i] != '\0') {
+    switch (src[i]) {
+      case '\t':
+        dest[j++] = '\\';
+        dest[j++] = 't';
+        break;
+      case '\n':
+        dest[j++] = '\\';
+        dest[j++] = 'n';
+        break;
+      default:
+        dest[j++] = src[i];
+        break;
+    }
+    ++i;
+  }
+  dest[j] = '\0';
+  return j;
+}
+
+int real(char dest[], char src[]) {
+  int i = 0;
+  int j = 0;
+  while (src[i] != '\0') {
+    switch (src[i]) {
+      case '\\':
+        switch (src[i + 1]) {
+          case 't':
+            dest[j++] = '\t';
+            ++i;
+            break;
+          case 'n':
+            dest[j++] = '\n';
+            ++i;
+            break;
+          default:
+            dest[j++] = '\\';
+            break;
+        }
+        break;
+      default:
+        dest[j++] = src[i];
+        break;
+    }
+    ++i;
+  }
+  dest[j] = '\0';
+  return j;
+}
